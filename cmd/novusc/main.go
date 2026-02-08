c9bf799e9048b4d47b034c7b98d67150a69f885d
MJDawson <66313685+MJDaws0n@users.noreply.github.com>
2026-02-08 00:57:32 +0000

    Started doing some work on the lexer

--- cmd/novusc/main.go
+++ cmd/novusc/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
+	"novus/internal/lexer"
+	"os"
 	// "novus/internal/codegen"
-	// "novus/internal/lexer"
 	// "novus/internal/parser"
 	// "novus/internal/semantic"
 	// "novus/internal/ast"
@@ -12,6 +13,73 @@ import (
 const VERSION = "0.1.0"
 
 func main() {
+	// Inititial print statements
 	fmt.Println("Novus Compiler V" + VERSION)
 	fmt.Println("Made by MJDawson. https://mjdawson.net & https://github.com/MJDaws0n")
+	printDebug("Using debug mode.")
+
+	// Get file to compile
+	var filePath string = os.Args[1]
+	printDebug("Building using: " + filePath)
+
+	// Check file exists
+	if !fileExists(filePath) {
+		fmt.Println("Error: File does not exist.")
+		os.Exit(1)
+	}
+
+	printDebug("File found, building: " + filePath)
+
+	// Get file content
+	var fileContent string
+	var fileContentError error
+	fileContent, fileContentError = getFileContent(filePath)
+
+	if fileContentError != nil {
+		fmt.Println("Error: Could not read file.")
+		fmt.Println("Error details: " + fileContentError.Error())
+		os.Exit(1)
+	}
+
+	printDebug("Starting lexing process...")
+	var tokens = lexer.Lex(fileContent)
+	printTokens(tokens)
+}
+
+/**
+* Prints a debug message to the console.
+* @param message The message to print.
+ */
+func printDebug(message string) {
+	fmt.Println("[DEBUG] " + message)
+}
+func printTokens(tokens []lexer.Token) {
+	for _, token := range tokens {
+		fmt.Printf("[DEBUG] Token: %s, Value: %s, Line: %d, Column: %d\n", token.Type, token.Value, token.Line, token.Column)
+	}
+}
+
+/**
+* Checks if a file exists at the given path.
+* @param filePath The path to the file to check.
+* @return true if the file exists, false otherwise.
+ */
+func fileExists(filePath string) bool {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+/**
+* Gets content of a file at the given path.
+* @param filePath The path to the file to read.
+* @return The content of the file as a string, or an error if the file cannot be read.
+ */
+func getFileContent(filePath string) (string, error) {
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
 }
